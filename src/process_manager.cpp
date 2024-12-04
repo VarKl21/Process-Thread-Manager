c89fffaa195cb349b7ac2f88c363b35547cf564e
VarKl21 <kavari.leonard@icloud.com>
2024-12-03 19:27:36 -0800

    Create process_manager.cpp

---
+++ src/process_manager.cpp
@@ -0,0 +1,9 @@
+#include "process_manager.h"
+
+void create_process() {
+    // Implement process creation logic here
+}
+
+void terminate_process() {
+    // Implement process termination logic here
+}
