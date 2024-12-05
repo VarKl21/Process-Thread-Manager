3a15a9aca8086eea09f1e855d76da7a5e8e51dc4
bgetnet <96029795+bgetnet@users.noreply.github.com>
2024-12-05 15:11:10 -0500

    Update ipc_manager.h

--- include/ipc_manager.h
+++ include/ipc_manager.h
@@ -1,9 +1,11 @@
 #ifndef IPC_MANAGER_H
-
 #define IPC_MANAGER_H
 
 void create_process_with_pipe();
 void send_message(const char* message);
-
+void parent_to_child_communication();
+void ipc_demo();
 #endif
+
+
 
